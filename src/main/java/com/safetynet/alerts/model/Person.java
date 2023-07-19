5579498eb38db815ec52959700c76a3ff842675f
Monsieur 486 <monsieur486@gmail.com>
2023-07-19 17:17:16 +0200

    Reformat code

--- src/main/java/com/safetynet/alerts/model/Person.java
+++ src/main/java/com/safetynet/alerts/model/Person.java
@@ -1,4 +1,11 @@
 package com.safetynet.alerts.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@NoArgsConstructor
 public class Person {
 }
