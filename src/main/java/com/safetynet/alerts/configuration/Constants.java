4b2620c4606917840dec9ef1b731cc316e328afb
Monsieur 486 <monsieur486@gmail.com>
2023-07-19 17:04:58 +0200

    Add json data file name constant

---
+++ src/main/java/com/safetynet/alerts/configuration/Constants.java
@@ -0,0 +1,8 @@
+package com.safetynet.alerts.configuration;
+
+public class Constants {
+    public static final String JSON_DATA_FILE_NAME = "json/data.json";
+
+    private Constants() {
+    }
+}
