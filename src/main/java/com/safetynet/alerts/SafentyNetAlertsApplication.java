ac7019caf45ff8284e990a774b9f7d1e34a3bdde
Monsieur 486 <monsieur486@gmail.com>
2023-07-19 16:43:38 +0200

    first commit

---
+++ src/main/java/com/safetynet/alerts/SafentyNetAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafentyNetAlertsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafentyNetAlertsApplication.class, args);
+    }
+
+}
