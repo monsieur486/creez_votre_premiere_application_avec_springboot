045126ae9a599f1b7f4fa6cc6617e90807e9f50b
Monsieur 486 <monsieur486@gmail.com>
2023-08-02 16:27:19 +0200

    add FirestationService test

---
+++ src/test/java/com/safetynet/alerts/service/jsonfile/FirestationServiceTest.java
@@ -0,0 +1,70 @@
+package com.safetynet.alerts.service.jsonfile;
+
+import com.safetynet.alerts.model.Firestation;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+class FirestationServiceTest {
+
+    FirestationService classToTest = new FirestationService();
+
+    @Test
+    void getAllFirestations() {
+        List<Firestation> firestationServiceList = classToTest.getAllFirestations();
+        assertNotNull(firestationServiceList);
+    }
+
+    @Test
+    void save() {
+        int initRecord = classToTest.getAllFirestations().size();
+        Firestation firestation = new Firestation(
+                "1 rue de Paris",
+                1
+        );
+        classToTest.save(firestation);
+        assertEquals(initRecord + 1, classToTest.getAllFirestations().size());
+    }
+
+    @Test
+    void update() {
+        Firestation firestation = new Firestation(
+                "908 73rd St",
+                2
+        );
+        boolean retour = classToTest.update(firestation);
+        assertTrue(retour);
+    }
+
+    @Test
+    void updateNotFound() {
+        Firestation firestation = new Firestation(
+                "xxx",
+                1
+        );
+        boolean retour = classToTest.update(firestation);
+        assertFalse(retour);
+    }
+
+    @Test
+    void delete() {
+        Firestation firestation = new Firestation(
+ "112 Steppes Pl",
+          4
+        );
+        boolean retour = classToTest.delete(firestation);
+        assertTrue(retour);
+    }
+
+    @Test
+    void deleteNotFound() {
+        Firestation firestation = new Firestation(
+                "xxx",
+                1
+        );
+        boolean retour = classToTest.delete(firestation);
+        assertFalse(retour);
+    }
+}
---
+++ src/test/java/com/safetynet/alerts/service/jsonfile/MedicalRecordServiceTest.java
@@ -0,0 +1,24 @@
+package com.safetynet.alerts.service.jsonfile;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MedicalRecordServiceTest {
+
+    @Test
+    void getAllMedicalrecords() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
